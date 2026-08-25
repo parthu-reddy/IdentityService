f676ef6cc1d97c0505f56af5253f06ff04819ec6
Parthu <parthudhanireddy@gmail.com>
2026-08-25 18:01:33 +0530

    Fix Application configuration scanning and add spring-cloud-starter-config

--- src/main/java/com/fooddelivery/identity/IdentityApplication.java
+++ src/main/java/com/fooddelivery/identity/IdentityApplication.java
@@ -9,8 +9,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication(
     scanBasePackages = {"com.fooddelivery.identity", "com.fooddelivery.common"}
 )
-@org.springframework.boot.autoconfigure.domain.EntityScan(basePackages = {"com.fooddelivery.identity", "com.fooddelivery.common.entity"})
-@org.springframework.data.jpa.repository.config.EnableJpaRepositories(basePackages = {"com.fooddelivery.identity", "com.fooddelivery.common.repository"})
+@org.springframework.boot.autoconfigure.domain.EntityScan(basePackages = {"com.fooddelivery.identity", "com.fooddelivery.common"})
+@org.springframework.data.jpa.repository.config.EnableJpaRepositories(basePackages = {"com.fooddelivery.identity", "com.fooddelivery.common"})
 public class IdentityApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(IdentityApplication.class, args);
